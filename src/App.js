348b38a00395e63a597126f45f0926ab102f8466
shoh4n <maffin.killer007@gmail.com>
2023-07-17 18:02:50 +0600

    Create Browser router and a Layout component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import Layout from './components/Layout/Layout';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />
+    }
+  ]);
   return (
     <div>
       <Header></Header>
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Shop from '../Shop/Shop';
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Shop />
+    </div>
+  );
+};
+
+export default Layout;
