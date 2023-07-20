2139f5383480b3781557afcc01c039f3728b8619
Shohan Sharoar <shohansharoar89@gmail.com>
2023-07-21 02:50:31 +0600

    Create products and saved cart loader and return multiple items

---
+++ src/loader/productAndCartLoader.js
@@ -0,0 +1,20 @@
+import { getStoredCart } from "../utilities/fakedb";
+
+export const productAndCartLoader=  async() => {
+  // get product
+  const productsData = await fetch( 'products.json' );
+  const products = await productsData.json();
+
+  // get cart
+  const savedCart = getStoredCart();
+  const previousCart = [];
+  for( const id in savedCart ){
+    const addedProduct = products.find( product => product.id === id );
+    if( addedProduct ){
+      const quantity = savedCart[ id ];
+      addedProduct.quantity = quantity;
+      previousCart.push( addedProduct );
+    }
+  }
+  return { products, previousCart };
+}
