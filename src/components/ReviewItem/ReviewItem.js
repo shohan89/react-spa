1ce32ded4eb4eb9873dc4a74ec59e244e55a49b8
Shohan Sharoar <shohansharoar89@gmail.com>
2023-07-21 03:29:11 +0600

    Implement Review Item style

---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './ReviewItem.css';
+
+const ReviewItem = ({ product }) => {
+  const { name, img, price, quantity } = product;
+  return (
+    <div className='review-item'>
+      <div>
+        <img src={ img } alt="" />
+      </div>
+      <div className="review-details-container">
+        <div className="review-details">
+          <p>{ name }</p>
+          <p><small>Price: { price }</small></p>
+          <p><small>Quantity: { quantity }</small></p>
+        </div>
+        <div className="delete-container">
+          <button>Delete</button>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default ReviewItem;
