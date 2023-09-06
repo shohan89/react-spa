2c5bd227dd5f7580538754aa2372b24179b71d9b
Shohan Sharoar <maffin.killer007@gmail.com>
2023-09-06 22:21:02 +0600

    Connect with Firebase for authentication

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMY7EoP3Q5qYFcn7AaUvvdZDgtIhthpWA",
+  authDomain: "react-spa-6ce63.firebaseapp.com",
+  projectId: "react-spa-6ce63",
+  storageBucket: "react-spa-6ce63.appspot.com",
+  messagingSenderId: "305438580856",
+  appId: "1:305438580856:web:49b75d6439ee3193c34d8d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
